3e866ee863be76e53e57ebad8e839a4d196fbf79
aphyuan <panhouyuanjustforfun@gmail.com>
2018-02-24 22:08:57 -0500

    set up level2

---
+++ level2/app.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+const express = require('express');
+const app = express();
+const path = require("path");
+var zipcode = "";
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
+
+app.get('/getzipcode', function(req, res){
+  zipcode = req.query.zipcode;
+  console.log(zipcode);
+  var searchRequest = {
+    term:'restaurants',
+    location: zipcode,
+    offset: offsetnum
+  };
+
+  const client = yelp.client(apiKey);
+
+
+  client.search(searchRequest).then(response => {
+    const results = response.jsonBody.businesses;
+    const prettyJson = JSON.stringify(results, null, 4);
+    var restlist = [];
+    for (var i = 0; i < offsetnum; i++){
+      var restname = results[i]["name"];
+      restlist.push(restname);
+    }
+    console.log(restlist);
+    var selectedres = getRest(restlist);
+    console.log(selectedres);
+    res.send(selectedres);
+  }).catch(e => {
+    console.log(e);
+  });
+
+
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+
+function getRest(param){
+  var randomRest = param[Math.floor(Math.random() * param.length)];
+  console.log(randomRest);
+  return randomRest;
+}
+
+
+// Place holder for Yelp Fusion's API Key. Grab them
+// from https://www.yelp.com/developers/v3/manage_app
+const apiKey = 'saLn3TWj2WtpMMzRv2rtqFztGgRNbmKhakyktpDE-dylPNcKtTZMqRGMRPElvRIpUuEAZR1UXIDPz-zYmLFB2QAtfidxNpusW7uclyMfVpPs0LFCfSxHmoSRGjiRWnYx';
+const offsetnum = 10;
--- level2/generate.js
+++ level2/generate.js
@@ -8,8 +8,39 @@ $(document).ready(function(){
 });
 
 function getRest(){
-  var restlist = ["Pho 11", "Panera Bread"];
+  var restlist = generateList();
   var randomRest = restlist[Math.floor(Math.random() * restlist.length)];
   console.log(randomRest);
   return randomRest;
 }
+
+
+
+
+ //https://github.com/Yelp/yelp-api-v3/blob/master/docs/api-references/autocomplete.md
+//yelp.autocomplete(text: 'Mc', latitude: 40.730610, longitude: -73.935242, }, function(error, data) {});
+
+//https://github.com/Yelp/yelp-api-v3/blob/master/docs/api-references/businesses-search-phone.md
+//yelp.phoneSearch({phone: "+12223334444"}, function(error, data) {});
+
+
+//https://github.com/Yelp/yelp-api-v3/blob/master/docs/api-references/businesses-id-reviews.md
+//yelp.businessesReviews('yelp-san-francisco', function(error, data) {});
+
+
+//https://github.com/Yelp/yelp-api-v3/blob/master/docs/api-references/businesses-id.md
+//yelp.businesses('yelp-san-francisco', function(error, data) {});
+
+
+//https://github.com/Yelp/yelp-api-v3/blob/master/docs/api-references/businesses-search.md
+//yelp.search({term: 'food', location: 'Montreal', function(error, data) {});
+
+
+// A promise based API is also available:
+// yelp.search({ term: 'food', location: '16803' })
+// .then(function (data) {
+//   console.log(data);
+// })
+// .catch(function (err) {
+//   console.error(err);
+// });
